8a270c9cbd85159a9c9b47d6082c2ba5fe9f30d6
Siarhei Mamykin <bymamykin@gmail.com>
2024-09-19 16:01:02 +0200

    Configuration. Added ui components from shadcn. Added header

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./button";
+export { Checkbox } from "./checkbox";
+export { Select } from "./select";
+export { Dialog } from "./dialog";
+export { Drawer } from "./drawer";
+export { Input } from "./input";
+export { Slider } from "./slider";
